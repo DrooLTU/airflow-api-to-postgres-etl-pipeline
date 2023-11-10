8951c69f95ff57e2092da8bcbbfbefd463600c3f
DrooLTU <dzastinaslt@gmail.com>
2023-11-10 15:33:18 +0200

    Placeholder DockerOperator, some deps installed

--- dags/loan_etl.py
+++ dags/loan_etl.py
@@ -1,4 +1,5 @@
-
+from airflow import DAG
+from airflow.providers.docker.operators.docker import DockerOperator
 
 # SET UP DB OPERATOR
 # CREATE TABLES
@@ -6,6 +7,19 @@
 
 # TWO OF THESE SO MUST BE MODULAR:
 # NEEDS A DOCKER OPERATOR TO RUN PYTHON SCRIPT IN CONTAINER
+
+fetch_data = DockerOperator(
+       task_id="fetch_data",
+       image="my-docker-image/name-here",
+       command=[
+           "fetch-data",                      
+           "--input_path",
+           "/data/input/loans/{{ds}}.json",
+           "--output_path",
+           "/data/output/loans/{{ds}}.csv",
+       ],
+       volumes=["/tmp/airflow/data:/data"],
+   )
 
 # TRANSFORM DATA TO STAR SCHEMA
 
