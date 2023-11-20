6a48cf184a2488d4823daea8f1b34a32a78b0290
DrooLTU <dzastinaslt@gmail.com>
2023-11-20 13:21:29 +0200

    Got Kaggle API to work

--- docker/images/fetch_data/scripts/fetch_data.py
+++ docker/images/fetch_data/scripts/fetch_data.py
@@ -1,1 +1,9 @@
-#!/usr/bin/env python 
+#!/usr/bin/env python
+import os
+import kaggle
+
+dataset_name = 'vikasukani/loan-eligible-dataset'
+
+
+
+kaggle.api.dataset_download_files(dataset_name, path='.', unzip=True)
