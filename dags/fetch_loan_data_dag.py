02040880c84a4dc0344bd3b4c02c07fa681ab453
DrooLTU <dzastinaslt@gmail.com>
2023-11-23 14:34:46 +0200

    Deleted unused parts, initial working data transform and save to postgres

--- dags/fetch_loan_data_dag.py
+++ dags/fetch_loan_data_dag.py
@@ -4,13 +4,18 @@ from dotenv import load_dotenv
 
 from airflow import DAG
 from airflow.providers.docker.operators.docker import DockerOperator
-from airflow.operators.python import PythonOperator
+from airflow.operators.python import PythonOperator, BranchPythonOperator
 from airflow.exceptions import AirflowException
 from airflow.models import Variable
+from airflow.providers.postgres.operators.postgres import PostgresOperator
+from airflow.hooks.base import BaseHook
+from airflow.hooks.postgres_hook import PostgresHook
 
 from docker.types import Mount
 
 import pandas as pd
+
+from sqlalchemy import create_engine
 
 from datetime import datetime, timedelta
 
@@ -30,6 +35,8 @@ VOLUME_NAME = f"{PROJECT_NAME}_kaggle-data-volume"
 KAGGLE_API_KEY = Variable.get("kaggle_api_key", default_var=None)
 KAGGLE_API_USERNAME = Variable.get("kaggle_api_username", default_var=None)
 
+LOAN_DB_NAME = 'loans'
+
 
 default_args = {
     "owner": "airflow",
@@ -39,9 +46,20 @@ default_args = {
 }
 
 
-def _transform_data(
-    root_path: str = "/opt/airflow/data", output_path: str = "/opt/airflow/data"
-):
+def _check_postgres_db_exists(**kwargs):
+    postgres_hook = PostgresHook(postgres_conn_id='MainPG')
+    conn = postgres_hook.get_conn()
+
+    query = f"SELECT 1 FROM pg_database WHERE datname='{LOAN_DB_NAME}';"
+
+    with conn.cursor() as cursor:
+        cursor.execute(query)
+        result = cursor.fetchone()
+
+    return result is not None
+
+
+def _transform_data(root_path: str = "/opt/airflow/data", output_path: str = "/opt/airflow/data", **kwargs):
     """
     Transfrom loan data into star schema.
 
@@ -55,17 +73,34 @@ def _transform_data(
     if not os.path.exists(output_path):
         print(f"Creating the folder {output_path}.")
         os.makedirs(output_path)
-    dfs = []
+
     folder_path = os.path.join(root_path, "*.csv")
     csv_files = glob.glob(folder_path)
 
+    postgres_conn = BaseHook.get_connection('LoansDB')
+
+    connection_str = f"postgresql://{postgres_conn.login}:{postgres_conn.password}@{postgres_conn.host}:{postgres_conn.port}/{postgres_conn.schema}"
+    engine = create_engine(connection_str)
+
     for csv_file in csv_files:
         df = pd.read_csv(csv_file)
-        dfs.append(df)
+        table_name_root = os.path.splitext(os.path.basename(csv_file))[0]
+        # Fact Table
+        fact_table_df = df[['Loan_ID', 'ApplicantIncome', 'CoapplicantIncome', 'LoanAmount', 'Loan_Amount_Term', 'Credit_History', 'Property_Area']]
+        fact_table_df.to_sql(f'{table_name_root}_fact_table', engine, if_exists='replace', index=False)
 
-    combined_df = pd.concat(dfs, ignore_index=True)
+        # Borrower Dimension Table
+        borrower_df = df[['Loan_ID', 'Gender', 'Married', 'Dependents', 'Education', 'Self_Employed']]
+        borrower_df.drop_duplicates(subset='Loan_ID', inplace=True)  # Deduplicate based on Loan_ID
+        borrower_df.to_sql(f'{table_name_root}_borrower_dimension', engine, if_exists='replace', index=False)
 
-    print(combined_df)
+        # Location Dimension Table
+        location_df = df[['Property_Area']]
+        location_df.drop_duplicates(inplace=True)  # Deduplicate
+        location_df.to_sql(f'{table_name_root}_location_dimension', engine, if_exists='replace', index=False)
+
+
+    print("Data written to database")
 
 
 with DAG(
@@ -74,9 +109,32 @@ with DAG(
     description="DAG to create loans database in PostgreSQL",
     schedule=timedelta(days=1),
 ) as dag:
-    fetch_data = DockerOperator(
-        task_id="fetch_data",
+    
+    check_db_task = PythonOperator(
+        task_id='check_db_task',
+        python_callable=_check_postgres_db_exists,
+        provide_context=True,
+        dag=dag,
+    )
+    
+    create_loans_db_task = PostgresOperator(
+        task_id='create_loans_db_task',
+        sql=f"CREATE DATABASE {LOAN_DB_NAME};",
+        postgres_conn_id='MainPG',
+        autocommit=True,
+    )
+
+    branch_task = BranchPythonOperator(
+        task_id='branch_task',
+        python_callable=lambda **kwargs: 'create_loans_db_task' if kwargs['ti'].xcom_push(key='check_db_result', value=False) is False else 'fetch_data_task',
+        provide_context=True,
+        dag=dag,
+    )
+
+    fetch_data_task = DockerOperator(
+        task_id="fetch_data_task",
         image="justinaslorjus/kaggle_fetch_dataset:1.0-3.11",
+        trigger_rule="none_failed",
         command=[
             "/bin/bash",
             "-c",
@@ -91,13 +149,18 @@ with DAG(
         ],
     )
 
-    transform_data = PythonOperator(
-        task_id="transform_data",
+    transform_data_task = PythonOperator(
+        task_id="transform_data_task",
         python_callable=_transform_data,
+        provide_context=True,
         op_kwargs={"output_path": "/opt/airflow/data/transformed"},
     )
 
-    fetch_data >> transform_data
+    check_db_task >> branch_task
+    branch_task >> [create_loans_db_task , fetch_data_task]
+    create_loans_db_task >> fetch_data_task >> transform_data_task
+    fetch_data_task >> transform_data_task
+    
 
 if __name__ == "__main__":
     dag.cli()
